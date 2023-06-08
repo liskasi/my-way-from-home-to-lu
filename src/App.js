a50cae4bd277df15e5d72bf8d0405a20f91a6bc7
Elizaveta Sirotina <elizaveta.sirotina@magebit.com>
2023-06-08 23:37:46 +0300

    removed Clock import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Introduction from './components/Introduction/Introduction';
 import styles from './App.module.css'
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
-import Clock from './components/Clock/Clock';
 
 function App() {
   const [language, setLanguage] = useState("en");
@@ -13,7 +12,6 @@ function App() {
       <Header language={language} setLanguage={setLanguage} />
       <Introduction language={language} />
       <Main language={language} />
-      <Clock />
     </div>
   );
 }
