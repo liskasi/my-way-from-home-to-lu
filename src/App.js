e726f228c60089d03ab2e2ac7c6107b81858eca5
Elizaveta Sirotina <elizaveta.sirotina@magebit.com>
2023-06-10 10:48:24 +0300

    added text

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, {useState} from 'react';
-import Introduction from './components/Introduction/Introduction';
-import styles from './App.module.css'
-import Header from './components/Header/Header';
-import Main from './components/Main/Main';
+import React, { useState } from "react";
+import Introduction from "./components/Introduction/Introduction";
+import styles from "./App.module.css";
+import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 function App() {
   const [language, setLanguage] = useState("en");
 
   return (
     <div className={styles.root}>
+      <p className={styles.msg}>
+        The website is mostly ready but there are some things that I still need
+        to adjust
+      </p>
       <Header language={language} setLanguage={setLanguage} />
       <Introduction language={language} />
       <Main language={language} />
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,14 +13,14 @@ const chapters = [
     image: "autumnVyborg",
     clock: {
       hours: 9,
-      minutes: 50
+      minutes: 50,
     },
     speechBubbles: [
       {
         owner: "me",
         text: {
           en: "Hey there! I'm Lisa, a 20-year-old from Vyborg, Russia. During my school years, I was all over the place trying out different things. I did gymnastics, attended music school where I successfully completed piano classes, played tennis, and took drawing and filmmaking classes—you name it! Additionally to school which I finished with distinction and honours, I studied English, Physics, and Informatics. With such a wide range of experiences, I ended up choosing to continue my journey in the field of ITC. I found it super exciting, especially because I had an amazing teacher who made our classes fascinating and taught me a ton.",
-          ru: "Привет!",
+          ru: "Привет! Я Лиза, мне 20 лет и я из Выборга. В школьные годы я всегда пробовала разные вещи: занималась гимнастикой, посещала музыкальную школу, где успешно закончила уроки игры на фортепиано, играла в теннис, занималась рисованием и киносъемкой — не перечислить! Помимо школы, которую я закончила с отличием, я изучала английский язык, физику и информатику. Имея такой широкий спектр опыта, я решил продолжить свое путешествие в области ИКТ. Для меня это было очень интересным, особенно потому, что у меня был замечательная учительца, которая делала наши занятия увлекательными и многому меня научила.",
         },
       },
     ],
@@ -34,7 +34,7 @@ const chapters = [
     month: "october",
     clock: {
       hours: 10,
-      minutes: 30
+      minutes: 30,
     },
     attributes: {
       mari: true,
@@ -44,7 +44,7 @@ const chapters = [
         owner: "me",
         text: {
           en: "This is my friend Mariia who graduated last year",
-          ru: "",
+          ru: "Это моя подруга Мария, которая закончила школу в прошлом году.",
         },
         timeout: 3500,
       },
@@ -52,7 +52,7 @@ const chapters = [
         owner: "mari",
         text: {
           en: "Hello Lisa, did you know that I’m studying in Vilnius, Lithuania?",
-          ru: "",
+          ru: "Привет, Лиза! Ты знала, что я учусь в Вильнюсе?",
         },
         timeout: 4000,
       },
@@ -60,7 +60,7 @@ const chapters = [
         owner: "me",
         text: {
           en: "She provided me with detailed information regarding studying abroad, including application requirements, pricing aspects, suggestions for potential universities, the benefits of studying in a foreign country, and comprehensive details about how Erasmus programs function. As I got more engrossed in the idea, studying abroad started feeling more achievable and realistic for me. Yet, I faced a dilemma—I couldn't decide whether to study in the absolutely lovely city of Saint Petersburg or venture out and explore a completely new place.",
-          ru: "",
+          ru: "Она дала мне подробную информацию об обучении за границей, включая требования к поступлению, цен, рекомендации университетов, преимущества обучения в другой стране, о том, как работает программа Erasmus. После её рассказа, учеба за границей стала казаться мне более достижимой и реалистичной. Тем не менее, у меня была дилемма — я не могла решить, учиться ли мне в любимом городе Санкт-Петербург или рискнуть и поехать в совершенно новое место».",
         },
       },
     ],
@@ -74,7 +74,7 @@ const chapters = [
     month: "december",
     clock: {
       hours: 11,
-      minutes: 55
+      minutes: 55,
     },
     attributes: {
       mother: true,
@@ -85,7 +85,7 @@ const chapters = [
         owner: "me",
         text: {
           en: "So, I decided to tell my mom about Mariia's story, and it turned out she got really inspired too.",
-          ru: "",
+          ru: "Я решила рассказать маме историю Марии, и оказалось, что она тоже очень вдохновилась.",
         },
         timeout: 4000,
       },
@@ -93,7 +93,7 @@ const chapters = [
         owner: "mother",
         text: {
           en: "Honestly, I had thought about sending my kids (Lisa and her brother) to study abroad before, but I never really considered the Baltic states as an option. Among our friends, Finland and Germany seemed to be the top choices for studying abroad.",
-          ru: "",
+          ru: "Если честно, я и раньше думала, чтоб отправить своих детей (Лизы и её брата) учиться за границу, но никогда не рассматривала страны Балтии как вариант. Среди наших друзей Финляндия и Германия казались лучшим выбором для обучения за границей.",
         },
         timeout: 15000,
       },
@@ -101,7 +101,7 @@ const chapters = [
         owner: "me",
         text: {
           en: "Excited by the idea, we started checking out universities in the Baltic countries and ended up with four choices: LU, RTU, and two universities in Kaunas, Lithuania. We were really drawn to these options because they had great locations, affordable prices, and interesting subjects, they also presented a wide range of opportunities.",
-          ru: "",
+          ru: "Воодушевленные этой идеей, мы начали проверять университеты в странах Балтии и остановились на четырех вариантах: ЛУ, РТУ и два университета в Каунасе, Литва. Нас очень привлекали эти варианты, потому что у них были отличные локации, доступные цены и интересные предметы, а также они давали широкий спектр возможностей.",
         },
       },
     ],
@@ -114,9 +114,17 @@ const chapters = [
     image: "springVyborg",
     clock: {
       hours: 12,
-      minutes: 12
+      minutes: 12,
     },
-    speechBubbles: [{ owner: "me", text: { en: "aaaaaaa", ru: "фывфыв" } }],
+    speechBubbles: [
+      {
+        owner: "me",
+        text: {
+          en: "I looked at the list of documents I needed to get ready: IELTS, motivational letter, recommendation letters, document verification, and a few others. I was supposed to take the IELTS exam in early March, but because of COVID, it kept getting postponed over and over again.",
+          ru: "Я взяла список документов, которые мне нужно было подготовить: IELTS, мотивационное письмо, рекомендательные письма, апостиль документов и некоторые другие. У меня должен был бы IELTS в начале марта, но из-за ковида его постоянно откладывали.",
+        },
+      },
+    ],
   },
   {
     title: {
@@ -127,9 +135,17 @@ const chapters = [
     month: "june",
     clock: {
       hours: 3,
-      minutes: 5
+      minutes: 5,
     },
-    speechBubbles: [{ owner: "me", text: { en: "asddsdar", ru: "фрфрфрр" } }],
+    speechBubbles: [
+      {
+        owner: "me",
+        text: {
+          en: "In the end, I managed to take the IELTS exam on June 15th, just two weeks before the application deadline (which was later extended). Generally, it went well. However, during the Speaking part, they asked me some strange questions about pens and pencils that caught me off guard because I wasn't prepared for that kind of question. But the important thing is that I received my IELTS certificate, and now I can go ahead and apply for university.",
+          ru: "В итоге мне удалось сдать экзамен IELTS 15 июня, всего за две недели до дедлайна подачи заявок (который позже был продлен). В целом, прошло хорошо. Однако во время разговорной части мне задали несколько странных вопросов о ручках и карандашах, которые застали меня врасплох, потому что я не была готов к такого рода вопросам. Но главное - я получила сертификат IELTS и теперь могу подаваться в университет.",
+        },
+      },
+    ],
   },
   {
     title: {
@@ -143,9 +159,17 @@ const chapters = [
     },
     clock: {
       hours: 7,
-      minutes: 40
+      minutes: 40,
     },
-    speechBubbles: [{ owner: "me", text: { en: "bla", ru: "фыв" } }],
+    speechBubbles: [
+      {
+        owner: "me",
+        text: {
+          en: "Unfortunately, it was the first year when COVID started, so our graduation didn't have many things going on. We got our diplomas, but there was no performance at school. I don't really regret it because preparing for it would take up a lot of time. Instead, we had a really nice unofficial gathering with our classmates and parents. We had a photo shoot, danced, and even had some competitions. However, there's an annual event in Saint Petersburg for graduating students called Scarlet Sails, and it was my dream to attend. But unfortunately, it was held remotely. \n As for the exams, for some reason, I decided to take five exams instead of the usual three that most people take. Before almost each exam, I took 10-day courses (five hours a day) to refresh my knowledge on the subjects. So, I had to attend 10 hours of these courses every day. It may sound a bit crazy, but I actually enjoyed those times and looking back, I think fondly of them. In the end, I achieved the highest score in the Math exam among all the students in my hometown. It's something I'm still proud of :)",
+          ru: "К сожалению, это был первый год, когда начался COVID, поэтому на нашем выпускном много вещей было отменено. Дипломы получали отдельно друг от друга, но какого-то мероприятия в школе не было. Я не очень жалею об этом, потому что подготовка к нему заняла бы много времени. Вместо этого у нас была очень приятная неофициальная встреча с одноклассниками и родителями. Мы устроили фотосессию, танцевали и участвовали в конкурсах от ведущего. \n В Санкт-Петербурге ежегодно проводится мероприятие для выпускников «Алые паруса», и я мечтала там побывать. Но, к сожалению, в 2020 он проходил дистанционно. \n Что касается экзаменов, почему-то я решила сдавать пять экзаменов вместо трёх, как сдают большинство. Почти перед каждым экзаменом я проходила 10-дневные курсы (по пять часов в день), чтобы повторить все темы по предметам. Мне приходилось каждый день заниматься по 10 часов. Это может звучать немного безумно, но мне действительно нравились те времена, и, оглядываясь назад, я вспоминаю их с теплом. В результате, я получила самый высокий балл на экзамене по математике в моём городе. И это то, чем я до сих пор горжусь :)",
+        },
+      },
+    ],
   },
   {
     title: {
@@ -159,9 +183,17 @@ const chapters = [
     },
     clock: {
       hours: 8,
-      minutes: 35
+      minutes: 35,
     },
-    speechBubbles: [{ owner: "me", text: { en: "asdads", ru: "длоав" } }],
+    speechBubbles: [
+      {
+        owner: "me",
+        text: {
+          en: "When I received the positive response from the university, my parents and I hurried to complete all the necessary paperwork so I could go to Latvia. Everything happened so fast... However, I still had to gather my belongings. It was challenging to decide what to take with me because it was uncertain when the borders would open. In the end, I had a 30kg suitcase. But then it turned out that I had to cross the border on foot. It was tough, but luckily, there were kind people who helped me carry my suitcase. When I finally arrived in Riga, the weather was amazing, and I instantly fell in love with the parks and the beautiful architecture. Everything was so picturesque and charming.",
+          ru: "Как только я получила положительный ответ от университета, мы с родителями побежали делать документы, чтобы я поехала в Латвию. Всё происходило быстро. Ещё надо было собрать вещи. Было сложно планировать, что нужно брать с собой, потому что было непонятно, когда границы откроются. Итого, 30кг чемодан готов. И вдруг оказалось, что границу надо будет переходить пешком. Было трудно, но мне много, кто помогал таскать чемодан. Когда я приехала в Ригу, была прекрасная погода, мне очень понравились парки и архитектура - всё было очень красиво.",
+        },
+      },
+    ],
   },
 ];
 
@@ -217,8 +249,7 @@ function Main({ language }) {
         language={language}
         clock={chapters[currentChapter].clock}
       />
-            {/* <Clock clock={chapters[currentChapter].clock} /> */}
-
+      {/* <Clock clock={chapters[currentChapter].clock} /> */}
     </div>
   );
 }
